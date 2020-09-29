9c6fc8ad94744ad8408587009b46648d3f5cd259
zyguss <43884096+zyguss@users.noreply.github.com>
2020-09-29 13:08:50 +0200

    Add files via upload

---
+++ jQuery&Ajax/13. jQuery Project!/main.js
@@ -0,0 +1,49 @@
+function updateOutput() {
+
+	 $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+	 document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+
+}
+
+// hover menja boju na prelazak misa i uklanja kad se skloni mis 
+$(".toggleButton").hover(function() {
+
+	 $(this).addClass("highlightedButton"); // highlightedButton klasa - js komunicira direktno sa css-om bez html-a
+
+}, function() {
+
+	 $(this).removeClass("highlightedButton");
+
+});
+
+$(".toggleButton").click(function() {
+
+	 $(this).toggleClass("active");
+
+	 $(this).removeClass("highlightedButton");
+
+	 var panelId = $(this).attr("id") + "Panel"; // stelujemo ime id-a (DIV) koji se uvek zavrsava sa panel (textarea)
+
+	 $("#" + panelId).toggleClass("hidden"); // i prosledjujemo ga ovde sa #
+
+	 var numberOfActivePanels = 4 - $('.hidden').length;
+
+	 $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
+
+})
+
+$(".panel").height($(window).height() - $("#header").height() - 15); // ***************
+
+$(".panel").width(($(window).width() / 2) - 10);
+
+updateOutput();
+
+$("textarea").on('change keyup paste', function() {
+
+	 updateOutput();
+
+
+});
+
+
